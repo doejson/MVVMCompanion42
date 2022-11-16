22ecb2eb9d0c70d664bd99ecdb6ab7a5cb8b9bb5
Tiana Patti <tpatti@at-k5.msk.21-school.ru>
2022-11-16 21:46:03 +0300

    Kek

---
+++ Companions/Companions/Model/IntraModel.swift
@@ -0,0 +1,8 @@
+//
+//  IntraModel.swift
+//  Companions
+//
+//  Created by Tiana Patti on 11/16/22.
+//
+
+import Foundation
---
+++ Companions/Companions/ViewModel/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  Companions
+//
+//  Created by Tiana Patti on 11/16/22.
+//
+
+import Foundation
